59700f774f83b46e053b5dc947237a1fd330c2a4
Losev Andrey <Losikoban@Gmail.com>
2019-07-08 20:09:02 +0300

    adaptive

--- src/site/blocks/default/nav/nav.js
+++ src/site/blocks/default/nav/nav.js
@@ -1,5 +1,22 @@
 var wrapperMenu = document.querySelector('.wrapper-menu');
+var menu = document.querySelector('.menu-item-block');
+
 
 wrapperMenu.addEventListener('click', function(){
+    toggleMenu();
+});
+
+function toggleMenu(){
     wrapperMenu.classList.toggle('open');
-})
+    if(menu.classList.contains('is--open')){
+        menu.classList.toggle('is--open');
+        setTimeout(function(){
+            menu.classList.toggle('is--dn');
+        },600);
+    }else{
+        menu.classList.toggle('is--dn');
+        setTimeout(function(){
+            menu.classList.toggle('is--open');
+        },200);
+    }
+}
